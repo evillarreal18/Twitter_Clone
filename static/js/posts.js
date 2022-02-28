1a3b041ed1193428a2f3d0bf504e94f190bd58e8
evillarreal18 <ericvillarreal81997@gmail.com>
2022-02-28 16:22:31 -0600

    updated

---
+++ static/js/posts.js
@@ -0,0 +1,12 @@
+///////////////////////////
+//
+// Javascript for Posts page
+//////////////////////////////
+
+$(function() {
+    $('.js-menu-icon').click(function() {
+        
+        $(this).next().toggle();
+    })
+
+})
